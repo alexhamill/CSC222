028aac8f7a31d1c3c19094dd8413b7937960595f
alexhamill <alexander.hamill@icloud.com>
2024-11-14 08:32:45 -0500

    part 3(added test cases)

--- secondquarter/test_num_vector_utils.cpp
+++ secondquarter/test_num_vector_utils.cpp
@@ -10,4 +10,16 @@ TEST_CASE("Testing render_num_vector") {
     string numstr = render_num_vector(nums);
     string expected = "1 3 7";
     CHECK(numstr == expected);
-} 
+}
+TEST_CASE("Testing multipul_render_num_vector") {
+    vector<int> nums = {1, 3, 7, 4, 5, 6};
+    string numstr = render_num_vector(nums);
+    string expected = "1 3 7 4 5 6";
+    CHECK(numstr == expected);
+}
+TEST_CASE("Testing no_render_num_vector") {
+    vector<int> nums = {};
+    string numstr = render_num_vector(nums);
+    string expected = "";
+    CHECK(numstr == expected);
+}

fe8888ce26690e8490c669e98fbdc84898047c76
alexhamill <alexander.hamill@icloud.com>
2024-11-22 00:17:25 -0500

    scafoding for point

---
+++ secondquarter/point/point.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "point.h"
+using namespace std;
+#include <string>
+
+Point::Point(){
+    x=0;
+    y=0;
+}
+Point::Point(int inx){
+    
+}
+Point::Point(int inx, int iny){
+    
+}
---
+++ secondquarter/point/point.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Point{
+    int x;
+    int y;
+
+    Point();
+    Point(int);
+    Point(int,int);
+};

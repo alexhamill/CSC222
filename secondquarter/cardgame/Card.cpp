85fc09c65b9ee8cb36b7ae68e6e93acc7f794eec
alexhamill <alexander.hamill@icloud.com>
2024-12-09 08:36:10 -0500

    made the cards work and to string

---
+++ secondquarter/cardgame/Card.cpp
@@ -0,0 +1,23 @@
+#include <doctest.h>
+#include <iostream>
+#include <string>
+#include "Card.h"
+using namespace std;
+
+Card::Card() {
+    suit = 0; rank = 0;
+}
+
+Card::Card(int s, int r) {
+    suit = s; rank = r;
+}
+string Card::to_string() const
+{
+    vector<string> suit_strings = {"None", "Clubs", "Diamonds",
+                                   "Hearts", "Spades"};
+    vector<string> rank_strings = {"Joker", "ace" , "2", "3", "4", "5", "6", "7",
+                                   "8", "9", "10", "Jack", "Queen", "King"};
+
+    if (rank == 0) return rank_strings[rank];
+    return rank_strings[rank] + " of " + suit_strings[suit];
+}
---
+++ secondquarter/cardgame/Card.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Card
+{
+    int suit, rank;
+
+    Card();
+    Card(int s, int r);
+    string to_string() const;
+};
